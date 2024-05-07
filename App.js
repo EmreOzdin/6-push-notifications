f2e2d470c843f417d1d54d0bcc6d8c982039240d
Emre Özdin <mrzdnboun@gmail.com>
2024-05-07 10:46:42 +0300

    "Add local notification scheduling in App.js and update package-lock.json and package.json for new dependencies"
    This diff adds a function to schedule a local notification in App.js using the Expo Notifications module. It

--- App.js
+++ App.js
@@ -1,10 +1,27 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { Button, StyleSheet, View } from "react-native";
+import * as Notifications from "expo-notifications";
 
 export default function App() {
+  function scheduleNotificationHandler() {
+    Notifications.scheduleNotificationAsync({
+      content: {
+        title: "My first Local Notification",
+        body: "This is the body of the notification",
+        data: { userName: "Emre" },
+      },
+      trigger: {
+        seconds: 5,
+      },
+    });
+  }
+
   return (
     <View style={styles.container}>
-      <Text>Naber lan tırrek!</Text>
+      <Button
+        title="Schedule Notification"
+        onPress={scheduleNotificationHandler}
+      />
       <StatusBar style="auto" />
     </View>
   );
